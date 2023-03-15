642dffe3f6edb1aa67430ad1e6fc01284afc61d6
UlrichMain <84636360+UlrichMain@users.noreply.github.com>
2023-03-15 12:17:45 +0200

    Setup of initial project architecture

---
+++ OnitamaSimulator/OnitamaSimulator/Board.py
@@ -0,0 +1,24 @@
+import Tile
+
+class Board:
+
+    """
+    A class used to represents the Onitama Board
+    ...
+
+    Attributes
+
+    Board board: 5 by 5 Array of Tiles
+
+    ----------
+    Methods
+    -------
+
+    """
+
+    #Attributes
+
+    board=None;
+
+    #Methods
+
---
+++ OnitamaSimulator/OnitamaSimulator/GameEngine.py
@@ -0,0 +1,36 @@
+import Board, Card
+class Game:
+
+    """
+    A class used to represent a single Onitama Game
+
+    ...
+
+    Attributes
+
+    Player: player1, player2 
+    Board: board 
+    Card: neutralCard
+
+    ----------
+    Methods
+    -------
+
+    Game(): Initiates game by distributing two cards to each player (Player.Cards) and neutralCard, gives each player a colour,
+            setups board, and enter while loop that lasts til someone wins the game
+
+    """
+
+    # Attributes
+    player1 = None
+    player2= None
+
+    board=None
+    neutralCard=None;
+
+
+    #Methods Other
+
+
+
+
