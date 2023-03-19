850b330e76a328e3a4d506726c8ec134aaa4bc2f
MZSFighters <127732471+MZSFighters@users.noreply.github.com>
2023-03-19 12:16:47 +0200

    Update Tile.py

--- OnitamaSimulator/OnitamaSimulator/Game/Tile.py
+++ OnitamaSimulator/OnitamaSimulator/Game/Tile.py
@@ -5,23 +5,38 @@ class Tile:
     A class used to represents a tile on the Onitama board
     ...
 
+    ----------
     Attributes
-
-    Boolean isSenseiChair: True if tile contains Sensei Chair
+    
     Piece piece: piece located on this tile
     Integer row, col
 
     ----------
     Methods
     -------
-
+       
+    isSenseiSeat(): This method checks if the tile at the given (row,col) coordinate is a master seat/tile or not. It returns true if it is.
+    
+    Value(): This method checks if a piece exists on the (row,col) coordinate. If yes it returns 1 otherwise a 0. This helps to print the grid.
     """
-
-    #Attributes
-
-    isSenseiChair = False
-    piece=None
-    row=None
-    col=None
-
+    
     #Methods
+    
+    def __init__(self, piece, row, col):
+       self.piece = piece
+       self.row = row
+       self.col = col
+       
+    def isSenseiSeat(self):
+        if(self.row == 0 and self.col == 2):
+            return True
+        elif(self.row == 4 and self.col == 2):
+            return True
+        else:
+            return False
+    
+    def Value(self):
+        if(self.piece == None):
+            return 0
+        else:
+            return 1
