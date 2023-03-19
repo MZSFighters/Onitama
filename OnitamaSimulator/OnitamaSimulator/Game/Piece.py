b6ecd7fb65bc42f7f27a3d013101485f839a8408
MZSFighters <127732471+MZSFighters@users.noreply.github.com>
2023-03-19 12:25:50 +0200

    Update Piece.py

--- OnitamaSimulator/OnitamaSimulator/Game/Piece.py
+++ OnitamaSimulator/OnitamaSimulator/Game/Piece.py
@@ -1,7 +1,17 @@
+import Player
+
+"""A class for the pawn piece
+     ----------
+     Attributes
+     
+     isMaster (Boolean) - To keep track of the master pawn piece
+     ----------
+     Methods
+"""
 
 
 class Piece:
-    pass
-
+    def __init__(self, isMaster, player):
+        self.isMaster = isMaster
 
 
