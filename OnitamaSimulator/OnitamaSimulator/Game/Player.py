da9e79dca128c5b3c6600184d5d7517c25319674
MZSFighters <127732471+MZSFighters@users.noreply.github.com>
2023-03-19 12:21:41 +0200

    Update Player.py

--- OnitamaSimulator/OnitamaSimulator/Game/Player.py
+++ OnitamaSimulator/OnitamaSimulator/Game/Player.py
@@ -4,15 +4,27 @@ class Player:
     """
     A class used to represent a player.
     ...
-
+    ----------
     Attributes
 
     Boolean Colour: Blue is True, red is False -> should be newly assigned each new game
-    Array(Card) Card: Array of the two given cards. 
+    Two Cards (String): Two cards that belong to this player. 
 
     ----------
     Methods
     -------
-
+    colour(): returns the colour of the player
+    
     """
-
+    
+    def __init__(self, name, colour, card1, card2):
+        self.name = name
+        self.colour = colour
+        self.card1 = card1
+        self.card2 = card2
+    
+    def colour(self):
+        if(self.colour == True):
+            return "Red"
+        else:
+            return "Blue"
