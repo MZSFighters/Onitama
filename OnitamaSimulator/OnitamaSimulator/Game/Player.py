43282fef4cff8415aecd486d2d660a97eb3cb3e9
UlrichMain <84636360+UlrichMain@users.noreply.github.com>
2023-03-23 22:46:30 +0200

    Update Player.py

--- OnitamaSimulator/OnitamaSimulator/Game/Player.py
+++ OnitamaSimulator/OnitamaSimulator/Game/Player.py
@@ -1,4 +1,5 @@
-import Card
+from Card import Card
+from Piece import Piece
 class Player:
 
     """
@@ -8,23 +9,46 @@ class Player:
     Attributes
 
     Boolean Colour: Blue is True, red is False -> should be newly assigned each new game
-    Two Cards (String): Two cards that belong to this player. 
+    card1, card2: The two cards that belong to the player
+    List(Pieces) pieces: A list of all Pieces beloning to a player
 
     ----------
     Methods
     -------
     colour(): returns the colour of the player
+    givePieces(): creates all the pieces belonging to a player and stores them in self.pieces
     
     """
-    
-    def __init__(self, name, colour, card1, card2):
+
+    def __init__(self, name="testName", colour= True):
         self.name = name
         self.colour = colour
-        self.card1 = card1
-        self.card2 = card2
-    
+        self.card1 = Card.selectCard()
+        self.card2 = Card.selectCard()
+        self.pieces = None
+
+
+    def givePieces(self):
+        '''Initializes the List of Pieces for a player'''
+        if (self.colour==True):
+            row=0
+        else:
+            row=4
+
+        pieceList =[]
+        for i in range(0,5):
+            if (i==2): #master piece     
+                pieceList.append(Piece(self.colour, True,row, i))
+            else:    
+                pieceList.append(Piece(self.colour, False,row, i))
+        
+        self.pieces=pieceList
+
+
+    #Utility Functions
     def colour(self):
         if(self.colour == True):
             return "Red"
         else:
             return "Blue"
+
