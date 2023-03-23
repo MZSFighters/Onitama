2c18fe2061a38f20ae7072e4c3e5e863314a8474
UlrichMain <84636360+UlrichMain@users.noreply.github.com>
2023-03-23 22:44:00 +0200

    Updated Board.py

--- OnitamaSimulator/OnitamaSimulator/Game/Board.py
+++ OnitamaSimulator/OnitamaSimulator/Game/Board.py
@@ -1,5 +1,6 @@
-import Tile
-import Piece
+from Tile  import Tile
+from Piece import Piece
+from Player import Player
 
 class Board:
 
@@ -17,40 +18,37 @@ class Board:
     -------
     
     printBoard(): prints the current state of the board as a 5 by 5 grid/matrix whenever the method is called upon.
+                  ** When you print the board (0,0) is the top left 
     
     initialise(): we can call initialise method to give us the initial state of the board when the user starts the game.
     
     """
 
-    
+    def __init__(self, player1, player2) -> None:
+        array = [[0, 0, 0, 0, 0] ,[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0]]   # dummy array
+        # Initiate Array to be 2d array of empty tiles
+
+        for i in range(0, 5):
+            for j in range(0,5):
+                array[i][j] = Tile(None, i, j)
+
+        for playerPiece in player1.pieces:
+            array[playerPiece.row][playerPiece.col].piece = playerPiece
+
+        for playerPiece in player2.pieces:
+            array[playerPiece.row][playerPiece.col].piece = playerPiece
+
+        self.arr = array
 
 
+    def returnTile(self, i,j):
+        '''Returns the tile at the position i,j on the board'''
+        return(self.arr[i][j])
 
-    def __init__(self, arr) -> None:
-        self.arr = arr
-    
     def printBoard(self):
         for i in range(5):
             for j in range(5):
                 print(str(self.arr[i][j].Value()),end=" ")           # arr[i][j] is the tile and we are calling a function of tile called Value()
             print("")
             
-            
-    def initialise(self):
-        array = [[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0]]   # dummy array
-        p = Piece(False, 1)         #isNotMaster pawn piece prototype
-        p2 = Piece(True, 1)         #isMaster pawn piece prototype
-        for i in range(5):
-            for j in range(5):
-                if(i ==0 or i==4):
-                    if(j == 2):
-                        t = Tile(p2, i, j)
-                    else:
-                        t = Tile(p, i, j)
-                    array[i][j] = t
-                else:
-                    t = Tile(None, i, j)
-                    array[i][j] = t
-        self.arr = array
-        return array                #returns an array containing tiles
 
