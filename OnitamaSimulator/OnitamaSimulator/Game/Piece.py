c64ac183e3ab05a1c94da1c0b6e2f8eeee334595
UlrichMain <84636360+UlrichMain@users.noreply.github.com>
2023-03-23 22:46:07 +0200

    Update Piece.py

--- OnitamaSimulator/OnitamaSimulator/Game/Piece.py
+++ OnitamaSimulator/OnitamaSimulator/Game/Piece.py
@@ -1,17 +1,28 @@
-import Player
-
 """A class for the pawn piece
      ----------
      Attributes
      
      isMaster (Boolean) - To keep track of the master pawn piece
+     colour (Boolean)
      ----------
      Methods
+
+     colour() return colour of the piece
 """
 
-
 class Piece:
-    def __init__(self, isMaster, player):
+    def __init__(self, colour, isMaster, row=0, col =0):
         self.isMaster = isMaster
+        self.colour = colour
+        self.row =row 
+        self.col =col
 
+    def setTile(self, tiles):
+        '''Place piece on a tile'''
 
+    def colour(self):
+        if(self.colour == True):
+            return "Red"
+        else:
+            return "Blue"
+        
