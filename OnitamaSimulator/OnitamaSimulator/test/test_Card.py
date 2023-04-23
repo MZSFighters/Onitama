a8ad6d25ee24bff765a92d178278bf34d979acba
UlrichMain <84636360+UlrichMain@users.noreply.github.com>
2023-04-23 15:17:22 +0200

    Update test_Card.py

--- OnitamaSimulator/OnitamaSimulator/test/test_Card.py
+++ OnitamaSimulator/OnitamaSimulator/test/test_Card.py
@@ -4,7 +4,7 @@ currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 sys.path.append( parentdir+ '\\GameEngine')
-from Card import Card
+from GameEngine.Card import Card
 
 class testCard(unittest.TestCase):
 
